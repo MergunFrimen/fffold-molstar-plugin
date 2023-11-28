9fdd4125a7b7a7e7aece956719f6f4a21976ef38
Dominik Tichy <tichydominik451@gmail.com>
2023-11-28 13:54:57 +0100

    changes

---
+++ src/extension/MolstarViewer.tsx
@@ -0,0 +1,62 @@
+import { ContextModel } from "../models/context-model";
+import { PluginUIComponent } from "molstar/lib/mol-plugin-ui/base";
+import { SequenceView } from "./SequenceView";
+import { PluginContextContainer } from "molstar/lib/mol-plugin-ui/plugin";
+import { Viewport, ViewportControls } from "molstar/lib/mol-plugin-ui/viewport";
+import { BackgroundTaskProgress } from "molstar/lib/mol-plugin-ui/task";
+import { Toasts } from "molstar/lib/mol-plugin-ui/toast";
+import {
+  AnimationViewportControls,
+  LociLabels,
+  StateSnapshotViewportControls,
+  TrajectoryViewportControls,
+  SelectionViewportControls,
+} from "molstar/lib/mol-plugin-ui/controls";
+import "molstar/lib/mol-plugin-ui/skin/light.scss";
+
+export function MolstarViewer({ context }: { context: ContextModel }) {
+  const MolstarViewport =
+    context.molstar.spec.components?.viewport?.view || DefaultViewport;
+  // const SequenceView =
+  //   context.molstar.spec.components?.sequenceViewer?.view || SequenceView;
+
+  return (
+    <div className="flex h-full w-full flex-col">
+      <div className="relative" style={{ height: "100px" }}>
+        <PluginContextContainer plugin={context.molstar}>
+          <SequenceView />
+        </PluginContextContainer>
+      </div>
+      <div className="relative grow">
+        <PluginContextContainer plugin={context.molstar}>
+          <MolstarViewport />
+        </PluginContextContainer>
+      </div>
+    </div>
+  );
+}
+
+export class DefaultViewport extends PluginUIComponent {
+  render() {
+    const VPControls =
+      this.plugin.spec.components?.viewport?.controls || ViewportControls;
+
+    return (
+      <>
+        <Viewport />
+        <div className="msp-viewport-top-left-controls">
+          <AnimationViewportControls />
+          <TrajectoryViewportControls />
+          <StateSnapshotViewportControls />
+        </div>
+        <SelectionViewportControls />
+        <VPControls />
+        <BackgroundTaskProgress />
+        <div className="msp-highlight-toast-wrapper">
+          <LociLabels />
+          <Toasts />
+        </div>
+      </>
+    );
+  }
+}
---
+++ src/models/context-model.ts
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import { PluginUIContext } from "molstar/lib/mol-plugin-ui/context";
+import {
+  DefaultPluginUISpec,
+  PluginUISpec,
+} from "molstar/lib/mol-plugin-ui/spec";
+
+export const pluginUISpec: PluginUISpec = {
+  ...DefaultPluginUISpec(),
+};
+
+export class ContextModel {
+  molstar: PluginUIContext;
+
+  async clear() {
+    await this.molstar.clear();
+  }
+
+  async load(url: string) {
+    console.log(url);
+    const response = await fetch(url);
+    const cif = await response.text();
+    const data = await this.molstar.builders.data.rawData({ data: cif });
+    const trajectory = await this.molstar.builders.structure.parseTrajectory(
+      data,
+      "pdb"
+    );
+    await this.molstar.builders.structure.hierarchy.applyPreset(
+      trajectory,
+      "default"
+    );
+  }
+
+  constructor() {
+    this.molstar = new PluginUIContext(pluginUISpec);
+    void this.molstar.init();
+
+    this.load("https://alphafold.ebi.ac.uk/files/AF-L8BU87-F1-model_v4.pdb");
+  }
+}
