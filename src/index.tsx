6fa97328b5cd974f3ed8f19a66a8e8dbf4562d56
Dominik Tichy <tichydominik451@gmail.com>
2023-12-13 00:24:06 +0100

    test app

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import TestApp from './TestApp.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+// ReactDOM.createRoot(document.getElementById('root')!).render(<TestApp />);
